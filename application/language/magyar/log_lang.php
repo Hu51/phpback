94915c387b8ddd4d998c504ceeb7a4b725828609
Szabó, Gergely <greg@hu51.hu>
2021-02-06 00:11:32 +0100

    Hungarian (magyar) translation
    can you create a dropdown for language select?

---
+++ application/language/magyar/log_lang.php
@@ -0,0 +1,18 @@
+<?php
+    $lang['log_commented'] = '%s, ötlethez hozzászólás érkezett';
+    $lang['log_comment_deleted'] = 'Törölt hozzászólás %s';
+    $lang['log_idea_approved'] = 'Ötlet elfogadása %s';
+    $lang['log_idea_deleted'] = 'Törölt ötlet %s';
+    $lang['log_idea_status'] = 'Ötlet állapota megváltoztatva, erről %s1 erre %s2';
+    $lang['log_user_registered'] = 'Felhasználó regisztrált';
+    $lang['log_new_idea'] = 'Új ötlet létrehozva';
+    $lang['log_idea_voted'] = 'Ötlet %s1 megszavazva %s2 szavazattal';
+    $lang['log_user_banned'] = 'Felhasználó %s1 bannolva, %s2 napra';
+    $lang['log_user_was_banned'] = 'Felhasználó %s bannolva';
+    $lang['log_user_unbanned'] = 'Bannolt felhaszánló tiltása %s';
+    $lang['log_settings'] = 'Beállítások szerkesztve';
+    $lang['log_user_admin'] = 'A felhasználó átállítva Adminisztrátorrá';
+    $lang['log_category_created'] = 'kategória létrehozva';
+    $lang['log_category_description'] = 'leírás módosítva';
+    $lang['log_category_changed'] = 'Kategória megváltoztatva \'%s1\' erre \'%s2\'';
+    $lang['log_category_deleted'] = 'Kategória %s törörlve';
